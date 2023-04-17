c4d155dd355d0ef1b021c002fc22d9298a0ad4c8
padli <pkhoirul@gmail.com>
2023-04-17 21:00:10 +0700

    validate file upload

--- controllers/postController.go
+++ controllers/postController.go
@@ -3,7 +3,11 @@ package controllers
 import (
 	"fmt"
 	"mime/multipart"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/padli/go-api-crud/initializers"
@@ -40,7 +44,27 @@ func PostCreate(c *gin.Context) {
 		return
 	}
 
-	errImg := c.SaveUploadedFile(req.Image, fmt.Sprintf("./public/%s", req.Image.Filename))
+	// Check file extension
+	extension := filepath.Ext(req.Image.Filename)
+	if extension != ".jpg" && extension != ".jpeg" && extension != ".png" {
+		c.JSON(400, gin.H{
+			"msg": "invalid file type",
+		})
+		return
+	}
+
+	// Check file size
+	var maxSizeFile int64
+	maxSizeFile = 2097152
+	if req.Image.Size > maxSizeFile {
+		c.JSON(400, gin.H{
+			"msg": "file size too large",
+		})
+		return
+	}
+
+	newFileName := fmt.Sprintf("%s_%s%s", time.Now().Format("20060102150405"), strings.Split(req.Image.Filename, ".")[0], filepath.Ext(req.Image.Filename))
+	errImg := c.SaveUploadedFile(req.Image, fmt.Sprintf("./public/%s", newFileName))
 
 	if errImg != nil {
 		c.JSON(500, gin.H{
@@ -49,12 +73,20 @@ func PostCreate(c *gin.Context) {
 		return
 	}
 
+	// url := c.Request.URL
+	protocol := "http"
+	if c.Request.TLS != nil {
+		protocol = "https"
+	}
+	host := c.Request.Host
+	newURL := fmt.Sprintf("%s://%s/public/%s", protocol, host, newFileName)
+
 	// Create post data
 	post := models.Post{
 		Title:    req.Title,
 		Body:     req.Body,
-		Image:    req.Image.Filename,
-		ImageUrl: "http://localhost:5000/public/" + req.Image.Filename,
+		Image:    newFileName,
+		ImageUrl: newURL,
 	}
 
 	result := initializers.DB.Create(&post)
@@ -170,11 +202,25 @@ func PostDelete(c *gin.Context) {
 	// Param
 	id := c.Param("id")
 
+	// Get the post
+	var post models.Post
+	if err := initializers.DB.First(&post, id).Error; err != nil {
+		c.JSON(404, gin.H{"msg": "post not found"})
+		return
+	}
+
 	// Delete the post
-	initializers.DB.Delete(&models.Post{}, id)
+	if err := initializers.DB.Delete(&post).Error; err != nil {
+		c.JSON(400, gin.H{"msg": "failed to delete post"})
+		return
+	}
+
+	// Delete the file
+	if err := os.Remove(fmt.Sprintf("./public/%s", post.Image)); err != nil {
+		c.JSON(400, gin.H{"msg": "failed to delete file"})
+		return
+	}
 
 	// Response
-	c.JSON(200, gin.H{
-		"msg": "deleted succesfully",
-	})
+	c.JSON(200, gin.H{"msg": "deleted successfully"})
 }
